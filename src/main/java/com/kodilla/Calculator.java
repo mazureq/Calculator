19c027726949bc2d238b67808ddecc056bd8af51
“mazureq <poczta.mazur@gmail.com>
2018-05-13 18:26:14 +0200

    second commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,23 @@
 package com.kodilla;
 
 public class Calculator {
+
+    double a = 25;
+    double b = 20;
+
+    public double minus(){
+        return a - b;
+    }
+
+    public double plus(){
+        return a + b;
+    }
+    public static void main(String args[]) {
+
+        Calculator myCalc = new Calculator();
+        double result1 = myCalc.minus();
+        double result2 = myCalc.plus();
+        System.out.println(result1 + " " + result2);
+
+    }
 }
--- src/main/java/com/kodilla/CalculatorApplication.java
+++ src/main/java/com/kodilla/CalculatorApplication.java
@@ -3,6 +3,8 @@ package com.kodilla;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.SQLOutput;
+
 @SpringBootApplication
 public class CalculatorApplication {
 
